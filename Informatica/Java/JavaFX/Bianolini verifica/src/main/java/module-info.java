902bf008d98de9223270c31aa2079c59aaf05ab3
Daniele <iosonogugol@gmail.com>
2026-03-13 08:27:37 +0100

    Delete Informatica/Java/JavaFX/Bianolini verifica directory

--- Informatica/Java/JavaFX/Bianolini verifica/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.bianolini.bianolini_verifica {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.bianolini.bianolini_verifica to javafx.fxml;
-    exports com.bianolini.bianolini_verifica;
-}
