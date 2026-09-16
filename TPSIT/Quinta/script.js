9c25099423414c51da4ddab10c71a8ce2c9b7879
Daniele Bianolini <daniele.bianolini.00@gmail.com>
2026-09-16 09:38:13 +0200

    creazione del lato js per gestire la tabella dei feedback

--- TPSIT/Quinta/script.js
+++ TPSIT/Quinta/script.js
@@ -2,3 +2,28 @@ const form = document.getElementByID("form");
 const formFeedback = document.getElementById("formFeedback");
 const messaggio = document.getElementById("messaggio");
 const tabellaFeedback = document.getElementByID("tabellaFeedback");
+
+formFeedback.addEventListener("submit", gestisciSubmit);
+
+const dati = [];
+
+function gestisciSubmit(event){
+    event.preventDefault();
+
+    const nome = document.getElementById("nome").value.trim();
+    const email = document.getElementById("email").value.trim();
+    const data = document.getElementById("data").value;
+    const ora = document.getElementById("ora").value;
+    const tipo = document.getElementById("tipo").value;
+    const messaggio = document.getElementById("messaggio").value.trim();
+    const checkbox = document.getElementById("checkbox").checked;
+
+    if(!nome||!email||!data||!ora||!tipo||!messaggi){
+        alert("Compila tutti i campi obbligatori")
+        return
+    }
+
+    //checkbox ? va a controllare se la checkbox è stata spuntata o no e, di conseguenza, da al valore "iscrizione" si o no
+    const iscrizione = checkbox ? "Si" : "No";
+
+}
