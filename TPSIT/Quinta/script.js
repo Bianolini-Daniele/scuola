5d9a8b4ec2c5f56f1da8848233890b003a5f3a06
Daniele Bianolini <daniele.bianolini.00@gmail.com>
2026-09-16 09:28:46 +0200

    inizio  creazione js

---
+++ TPSIT/Quinta/script.js
@@ -0,0 +1,4 @@
+const form = document.getElementByID("form");
+const formFeedback = document.getElementById("formFeedback");
+const messaggio = document.getElementById("messaggio");
+const tabellaFeedback = document.getElementByID("tabellaFeedback");
