4709eb866f92d188300be2995f71fb8b99ceaa0b
Daniele Bianolini <daniele.bianolini.00@gmail.com>
2026-09-18 12:50:16 +0200

    .

--- TPSIT/Quinta/Prova_feedback/script.js
+++ TPSIT/Quinta/Prova_feedback/script.js
@@ -1,6 +1,4 @@
-const form = document.getElementByID("form");
 const formFeedback = document.getElementById("formFeedback");
-const messaggio = document.getElementById("messaggio");
 const tabellaFeedback = document.getElementByID("tabellaFeedback");
 
 formFeedback.addEventListener("submit", gestisciSubmit);
